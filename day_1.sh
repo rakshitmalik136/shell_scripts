7bf03335948a9625c830575571e3a84a2c7cad83
Rakshit Malik <111422125+rakshitmalik136@users.noreply.github.com>
2024-10-18 20:12:00 +0530

    Create day_1.sh

---
+++ day_1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Task-01: To create a comment
+#This is Day-1 of BashBlaze Let's get started...
+echo" "
+
+#Task-02: To write a statement through echo command
+echo "I'M INTO THIS CHALLENGE"
+echo " "
+
+#Task-03: To declare Variables
+var1="hello"
+var2="good morning!"
+echo " "
+
+#Task-04: To perform sum form declared values to variables
+greetings="$var1, $var2"
+echo "$greetings welcome to day-01 of bashblaze challenge"
+echo " "
+
+#Task-05: Use Build-In Variable
+uptime
+echo " "
+time
+echo " "
+date
+echo " "
+hostname
+echo " "
+
+#Task-06: Wildcards
+echo "Files with .txt extension in current directory"
+ls *.txt
+echo " "
